a9d3d6ef56b133209cac54da401830c2a6e7b218
dedeibel <dedeibel@arcor.de>
2017-12-19 22:27:00 +0100

    34c3 added phasenpruefer cursor

--- configs/conferences/34c3/assets/svggen/generate-headlines.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in "34C3 Streaming" "beta" "Feedback" "Info" "Live Music" "Live" "Specials" "Recordings" "Releases" "TLS" "Relive"; do
-	echo "$i"
-	bash generate-headline.sh "$i"
-done
